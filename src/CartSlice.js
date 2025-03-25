ccb75f7201510ae3ea0a4c8d8b9534718f825cc2
laladiaz <mariaabelardadiaz@gmail.com>
2025-03-24 20:51:58 -0600

    it created the CartSlice file

---
+++ src/CartSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cartSlice = createSlice ({
+    name: 'cart',
+    initialState: [],
+    redurcers:{}
+});
+
+//export const {  } = cartSlice.actions;
+
+//export default cartSlice.reducer;
